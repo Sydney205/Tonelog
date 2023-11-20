87b621b9767f8e9339201501321f077b70087d7b
Sydney205 <diekechi@gmail.com>
2023-11-20 14:13:03 +0100

    # Feature: tone, timestamp & typing effect

--- index.js
+++ index.js
@@ -1,7 +1,111 @@
-const colors = require("./colors");
+const { colors, addColor, logLevels } = require("./styles");
 
-const tonelog = (level, message) => {
-  console.log(`${colors[level]}${message}${colors.reset}`);
+// using color Log
+const colorLog = () => {
+  const colorList = {};
+
+  for (const color in colors) {
+    colorList[color] = (message) => {
+      return addColor(color, message);
+    };
+  }
+
+  return colorList;
 };
 
-module.exports = tonelog;
+const tone = colorLog();
+
+// Using Timestamp
+const useTimestamp = (message) => {
+  let timestamp = new Date().toLocaleTimeString();
+  return `${tone.bright_white(`[${timestamp}]`)} - ${message}`;
+};
+
+// create Log levels
+const createLevelLog = () => {
+  let Options = {};
+
+  logLevels.forEach((level) => {
+    Options[level] = (message) => {
+      const msgColor = addColor(level, message); // Coloring the message according to it's level
+      return `${tone[`bg_${level}`](` ${level.toUpperCase()} `)} ${msgColor}`;
+    };
+  });
+
+  return Options;
+};
+
+const toneLevel = createLevelLog();
+
+// Typing effect
+const typed = (
+  strings = [],
+  delay = 1000,
+  typeSpeed = 50,
+  backspaceSpeed = 50
+) => {
+  return new Promise((resolve) => {
+    let index = 0;
+
+    const startTyping = () => {
+      if (index < strings.length) {
+        const string = strings[index];
+        let counter = 0;
+
+        setTimeout(() => {
+          // Typing
+          const typeInterval = setInterval(() => {
+            process.stdout.write(string[counter]);
+            counter++;
+
+            if (counter == string.length) {
+              clearInterval(typeInterval);
+
+              // Checking if it's the last string
+              if (index < strings.length - 1) {
+                // Start backspacing
+                setTimeout(() => {
+                  const backspaceInterval = setInterval(() => {
+                    process.stdout.write("\b \b");
+                    counter--;
+
+                    if (counter == 0) {
+                      clearInterval(backspaceInterval);
+
+                      // Moving on to the next string
+                      index++;
+                      startTyping();
+                    }
+                  }, backspaceSpeed);
+                }, delay);
+              } else {
+                // Resolve immediately for the last string
+                resolve();
+                console.log("");
+              }
+            }
+          }, typeSpeed);
+        }, delay);
+      } else {
+        // Resolve if there are no strings to type
+        resolve();
+      }
+    };
+
+    // Start typing the first string
+    startTyping();
+  });
+};
+
+// Old Style
+function tonelog(color, message) {
+  console.log(`${colors[color]}${message}${colors.reset}`);
+}
+
+module.exports = {
+  tone,
+  toneLevel,
+  useTimestamp,
+  typed,
+  tonelog,
+};
